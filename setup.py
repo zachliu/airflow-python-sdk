cdd930b25d9c9ebd22d4f4fcb40d2864d5252213
zachliu <zach.z.liu@gmail.com>
2021-04-02 12:18:34 -0400

    bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "airflow-python-sdk"
-VERSION = "0.18.13"
+VERSION = "1.0.0"
 # To install the library, run the following
 #
 # python setup.py install
